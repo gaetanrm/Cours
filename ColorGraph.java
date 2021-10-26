692b461b0ca17ca091bf871f9e41932c6f62bfd9
gaetanrm <gaetan@LAPTOP-D79UEVHJ.localdomain>
2021-10-26 17:47:06 +0200

    Update

--- ColorGraph.java
+++ ColorGraph.java
@@ -4,21 +4,37 @@ class ColorGraph {
 
     public Graphe createGraph() {   //On utlise le graphe du cours(ici celui en diamant), faudra faire aussi l'autre...
         ArrayList<String> node = new ArrayList<String>(); 
-        String listNode = "x, y, z, t";
-        node.add(listNode);
-        ArrayList<String> arete = new ArrayList<String>();
-        arete.add("xy");    //Doit y avoir mieux qu'ajouter comme ca la main
-        arete.add("xz");
-        arete.add("yt");
-        arete.add("zt");
-        ArrayList<String> aretePref = new ArrayList<String>();
+        node.add("x");      //Doit y avoir mieux qu'ajouter comme ca la main
+        node.add("y");
+        node.add("z");
+        node.add("t");
+        ArrayList<Pair<String, String>> arete = new ArrayList<Pair<String, String>>();
+        arete.add(new Pair<>("x", "y"));    //idem
+        arete.add(new Pair<>("x", "z"));
+        arete.add(new Pair<>("y", "t"));
+        arete.add(new Pair<>("z", "t"));
+        ArrayList<Pair<String, String>> aretePref = new ArrayList<Pair<String, String>>();
         return new Graphe(node, arete, aretePref);
 
     }
 
     public void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        int k;
+        System.out.println("Quel \"k\" voulez vous pour l'algo ? ");
+        k = keyboard.nextInt();
         Graphe graph = createGraph();
+        ColorAlgo(graph, k);
         graph.print();
+        keyboard.close();
+    }
+
+    public void ColorAlgo(Graphe graph, int k){
+        ArrayList<String> nodeSpill = new ArrayList<String>();  //Liste des noeuds spillé
+        ArrayList<String> nodeColor = new ArrayList<String>();  //Liste des noeuds colorés
+
+
+
     }
 
 }
--- Graphe.java
+++ Graphe.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class  Graphe {
     private ArrayList<String> node = new ArrayList<>(); //Pour tous les noeuds du graphe
-    private ArrayList<String> arete = new ArrayList<>(); //Pour les arêtes
-    private ArrayList<String> aretePref = new ArrayList<>(); //Pour les arêtes de préférences
+    private ArrayList<Pair<String, String>> arete = new ArrayList<Pair<String, String>>(); //Pour les arêtes
+    private ArrayList<Pair<String, String>> aretePref = new ArrayList<Pair<String, String>>(); //Pour les arêtes de préférences
 
-    public Graphe(ArrayList<String> node, ArrayList<String> arete, ArrayList<String> aretePref){
+    public Graphe(ArrayList<String> node, ArrayList<Pair<String, String>> arete, ArrayList<Pair<String, String>> aretePref){
         this.node = node;
         this.arete = arete;
         this.aretePref = aretePref;
@@ -19,12 +19,25 @@ class  Graphe {
         return node;
     }
 
-    public ArrayList<String> getArete(){
+    public ArrayList<Pair<String, String>> getArete(){
         return arete;
     }
 
-    public ArrayList<String> getAretePref(){
+    public ArrayList<Pair<String, String>> getAretePref(){
         return aretePref;
+    }
+
+    public ArrayList<Integer> counter(){
+        ArrayList<Integer> cmpt = new ArrayList<Integer>(); //Dans cette liste, les valeurs pour chaque noeuds ont le meme indice que dans la liste node
+        for(String e : this.node){
+            int cnt = 0;
+            for(int i=0; i < arete.size()-1; i++){
+                if (arete.get(i).left.contains(e))
+                    cnt++;
+                else if(arete.get(i).right.contains(e))
+                    cnt++;
+            }cmpt.add(cnt);
+        } return cmpt;
     }
 
 }
