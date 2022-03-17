d3e1ef8784270f1af336991e3e9a8142e07d57c5
gaetanrm <gaetanrm@github.com>
2022-03-17 18:43:23 +0100

    Continuation of TP3 and adding of Postgresql Database

--- modularité/tp3/src/main/java/fr/romero/tp3/Models/Location.java
+++ modularité/tp3/src/main/java/fr/romero/tp3/Models/Location.java
@@ -1,11 +1,14 @@
 package fr.romero.tp3.Models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @Entity(name="locations")
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Access(AccessType.FIELD)
 public class Location {
 
@@ -14,15 +17,13 @@ public class Location {
     private List<User> users;
 
     @Id
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    private Long location_id;
+    @Column
+    private int latitude;
+    @Column
+    private int longitude;
+    @Column
+    private Date location_date;
 
     public List<User> getUsers() {
         return users;
@@ -30,5 +31,37 @@ public class Location {
 
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public Long getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(Long location_id) {
+        this.location_id = location_id;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public Date getLocation_date() {
+        return location_date;
+    }
+
+    public void setLocation_date(Date location_date) {
+        this.location_date = location_date;
     }
 }
